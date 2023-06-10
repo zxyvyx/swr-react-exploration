c7a90b286659b6360e660369a2c836aa36768758
Hasymi Al <98.hasymi.prime@gmail.com>
2023-06-10 21:17:13 +0700

    feat: add sticky position of navbar
    Signed-off-by: Hasymi Al <98.hasymi.prime@gmail.com>

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -10,7 +10,7 @@ interface NavigationBarProps {
 
 export default function Navbar({ navigationList }: NavigationBarProps) {
   return (
-    <Disclosure as='nav' className='bg-gray-800'>
+    <Disclosure as='nav' className='bg-gray-800 sticky top-0 z-50'>
       {({ open }) => (
         <>
           <div className='max-w-7xl mx-auto px-2 sm:px-6 lg:px-8'>
