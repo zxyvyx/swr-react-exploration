f2afb1c94e713d503265899d76af467a49f522ff
Hasymi Al <98.hasymi.prime@gmail.com>
2023-06-11 00:18:03 +0700

    feat: add content to users page
    Signed-off-by: Hasymi Al <98.hasymi.prime@gmail.com>

---
+++ src/libs/interfaces/Users.ts
@@ -0,0 +1,71 @@
+import { Color, EyeColor, Gender, Type } from '../types/UserType';
+
+export interface UserResponses {
+  users: User[];
+  total: number;
+  skip: number;
+  limit: number;
+}
+
+export interface User {
+  id: number;
+  firstName: string;
+  lastName: string;
+  maidenName: string;
+  age: number;
+  gender: Gender;
+  email: string;
+  phone: string;
+  username: string;
+  password: string;
+  birthDate: Date;
+  image: string;
+  bloodGroup: string;
+  height: number;
+  weight: number;
+  eyeColor: EyeColor;
+  hair: Hair;
+  domain: string;
+  ip: string;
+  address: Address;
+  macAddress: string;
+  university: string;
+  bank: Bank;
+  company: Company;
+  ein: string;
+  ssn: string;
+  userAgent: string;
+}
+
+export interface Address {
+  address: string;
+  city?: string;
+  coordinates: Coordinates;
+  postalCode: string;
+  state: string;
+}
+
+export interface Coordinates {
+  lat: number;
+  lng: number;
+}
+
+export interface Bank {
+  cardExpire: string;
+  cardNumber: string;
+  cardType: string;
+  currency: string;
+  iban: string;
+}
+
+export interface Company {
+  address: Address;
+  department: string;
+  name: string;
+  title: string;
+}
+
+export interface Hair {
+  color: Color;
+  type: Type;
+}
---
+++ src/libs/types/UserType.ts
@@ -0,0 +1,7 @@
+export type EyeColor = 'Green' | 'Brown' | 'Gray' | 'Amber' | 'Blue';
+
+export type Gender = 'male' | 'female';
+
+export type Color = 'Black' | 'Blond' | 'Brown' | 'Chestnut' | 'Auburn';
+
+export type Type = 'Strands' | 'Curly' | 'Very curly' | 'Straight' | 'Wavy';
