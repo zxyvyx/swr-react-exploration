12456fe23c619cf40ba0b17c58d21b79bbaf3246
Hasymi Al <98.hasymi.prime@gmail.com>
2023-06-11 10:13:05 +0700

    chore: add debounce hook function for string
    Signed-off-by: Hasymi Al <98.hasymi.prime@gmail.com>

---
+++ src/hooks/useDebounceString.tsx
@@ -0,0 +1,17 @@
+/* eslint-disable import/prefer-default-export */
+import { useState, useEffect } from 'react';
+
+export const useDebounceString = (value: string, delay: number) => {
+  const [debouncedValue, setDebouncedValue] = useState(value);
+
+  useEffect(() => {
+    const handler = setTimeout(() => {
+      setDebouncedValue(value);
+    }, delay);
+    return () => {
+      clearTimeout(handler);
+    };
+  }, [value, delay]);
+
+  return debouncedValue;
+};
